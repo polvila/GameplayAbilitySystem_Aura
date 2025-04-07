2dd86c1ca955a476694f788ea4a99d4f2a6ee082
Pol Vilà Saló <polvilasalo@gmail.com>
2025-04-07 14:52:32 +0200

    Show health globe and mana globe UI to the HUD

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Take Away Games
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	OnWidgetControllerSet();
+}
